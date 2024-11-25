91710216e70ccbb99285053c8bfd33a05d3a3815
Ivan Alvarez <sistemas.ialvarez93@gmail.com>
2024-11-25 10:03:01 -0400

    feat(company): add company panel
    Basic configuration following the documentation

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers\Filament;
 
+use Filament\Navigation\MenuItem;
+use Filament\Navigation\NavigationItem;
+use Illuminate\Support\Facades\Auth;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -17,6 +20,8 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Wallo\FilamentCompanies\Pages\User\PersonalAccessTokens;
+use Wallo\FilamentCompanies\Pages\User\Profile;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -34,6 +39,24 @@ class AdminPanelProvider extends PanelProvider
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
                 Pages\Dashboard::class,
+                Profile::class,
+                PersonalAccessTokens::class,
+            ])
+            ->userMenuItems([
+                'profile' => MenuItem::make()
+                    ->label('Profile')
+                    ->icon('heroicon-o-user-circle')
+                    ->url(static fn() => url(Profile::getUrl())),
+                MenuItem::make()
+                    ->label('Company')
+                    ->icon('heroicon-o-building-office')
+                    ->url(static fn() => url(Pages\Dashboard::getUrl(panel: 'company', tenant: Auth::user()->personalCompany()))),
+            ])
+            ->navigationItems([
+                NavigationItem::make('Personal Access Tokens')
+                    ->label(static fn(): string => __('filament-companies::default.navigation.links.tokens'))
+                    ->icon('heroicon-o-key')
+                    ->url(static fn() => url(PersonalAccessTokens::getUrl())),
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
